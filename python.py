c87b53319c6ea300346e13f2016926946ef8217f
zizohosny211-arch <zizohosny211@gmail.com>
2025-11-11 20:58:24 +0200

    Fix typo and add text processing logic

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
 frist
+text = "Go to the next room and shut the door," " while doing so don't forget to get the keys" ", if got your parent's keys" ", don't forget to close the windows as well"
+
+words = tuple(text.split())
+
+for word in words:
+    for letter in word:
+        if letter.lower() == 'o':
+            print("MR.Zizo")
+
+
